ff8ca6639dfcefc40a7dca3a7079e4157ccae679
thiagocwbpr <thiagocwbpr@gmail.com>
2023-04-15 11:19:47 -0300

    Finalizado a estrutura de domínio do código.

--- PaymentContext.Domain/Entities/Pyament.cs
+++ PaymentContext.Domain/Entities/Pyament.cs
@@ -2,7 +2,7 @@ namespace PaymentContext.Domain.Entities
 {
     public abstract class Payment{
         
-        public string Number { get; set; }
+        public string Number { get; set; } // numero interno de controle. 8 digitos.
         public DateTime PaidDate { get; set; }
         public DateTime ExpireDate { get; set; }
         public decimal Total { get; set; }
@@ -10,18 +10,23 @@ namespace PaymentContext.Domain.Entities
         public string Owner { get; set; }
         public string Document { get; set; }
         public string Address { get; set; }
+        public string Email { get; set; }
 
     }
 
      public class BoletoPayment : Payment{
- 
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; } // gerado pela entidade. 12 digitos.
     }
 
      public class CreditCardPayment : Payment{
- 
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransactionNumber { get; set; }
     }
 
      public class PayPalPayment : Payment{
- 
+        
+        public string TransactionCode { get; set; }
     }
 }
