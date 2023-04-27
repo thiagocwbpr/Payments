d1a5e5dd941f6825be2f908d3a1cea739530a531
thiagocwbpr <thiagocwbpr@gmail.com>
2023-04-27 11:20:16 -0300

    Criado os Handler de Boleto, PayPal e Cartão de Credito.

--- PaymentContext.Domain/Handlers/SubscriptionHandler.cs
+++ PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -13,7 +13,8 @@ namespace PaymentContext.Domain.Handlers
 {
     public class SubscriptionHandler : Notifiable<Notification>, 
     IHandler<CreateBoletoSubscriptionCommand>,
-    IHandler<CreatePayPalSubscriptionCommand>
+    IHandler<CreatePayPalSubscriptionCommand>,
+    IHandler<CreateCreditCardSubscriptionCommand>
     {
         private readonly IStudentRepository _repository;
         private readonly IEmailService _emailService;
@@ -23,6 +24,8 @@ namespace PaymentContext.Domain.Handlers
             _repository = repository;
             _emailService = emailService;
         }
+
+        // Handler de Boleto
         public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
         {
             // Fail Fast Validations
@@ -140,6 +143,79 @@ namespace PaymentContext.Domain.Handlers
             // Somente muda a implementação do pagamento, abaixo:
             var payment = new PayPalPayment(
             command.TransactionCode,
+            command.PaidDate, 
+            command.ExpireDate,
+            command.Total,
+            command.TotalPaid, 
+            command.Payer,new Document(
+            command.Document, 
+            command.PayerDocumentType), address, email);
+
+            // Relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            // Aplicar as validações
+            AddNotifications(name, document,email, address, student,subscription,payment);
+
+            // Salvar as informações
+            _repository.CreateSubscription(student);
+
+            // Enviar email de boas vindas.
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "Bem Vindo ao nosso portal", "Sua assinatura foi criada.");
+
+            // Retornar informações
+
+            return new CommandResult(true, "Assinatura realizada com sucesso.");
+        }
+
+         // Handler de CreditCard
+
+        public ICommandResult Handle(CreateCreditCardSubscriptionCommand command)
+        {
+            // Fail Fast Validations
+            
+            // Verificar se documento já está cadastrado.
+            if(_repository.DocumentExists(command.Document))
+            AddNotification("Document", "Esse CPF já está cadastrado.");
+            
+            // { <-- Você pode fazer esse tipo de notificação também.
+            //  AddNotifications(command);
+            //  return new CommandResult(false, "CPF já em uso. ");  
+            // }
+
+            // Verificar se email já está cadastrado.
+            if(_repository.EmailExists(command.Email))
+            AddNotification("Email", "Esse E-mail já está cadastrado.");
+            
+            // { <-- O mesmo para esse, pode ser utilizado.
+            //  AddNotifications(command);
+            //  return new CommandResult(false, "Não foi possível realizar sua assinatura");  
+            // }
+            
+
+            // Gerar os VOs
+           var name = new Name(command.FirstName, command.LastName);
+           var document = new Document(command.Document, EDocumentType.CPF);
+           var email = new Email(command.Email);
+           var address = new Address(
+           command.Street,
+           command.Number,
+           command.Neighborhood,
+           command.City,
+           command.State,
+           command.Country,
+           command.ZipCode);
+
+            // Gerar as entidades
+            var student = new Student(name,document,email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+
+            // Somente muda a implementação do pagamento, abaixo:
+            var payment = new CreditCardPayment(
+            command.CardHolderName,
+            command.CardNumber,
+            command.LastTransactionNumber,
             command.PaidDate, 
             command.ExpireDate,
             command.Total,
