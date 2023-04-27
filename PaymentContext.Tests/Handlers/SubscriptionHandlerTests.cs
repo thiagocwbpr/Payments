ab772ac83e34b478d04b4a2bdfaa7c790a7430e4
thiagocwbpr <thiagocwbpr@gmail.com>
2023-04-27 12:48:25 -0300

    Finalizado os testes dos handlers.

--- PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -19,6 +19,30 @@ public class SubscriptionHandlerTests
         
         var handler = new SubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
         var command = new CreateBoletoSubscriptionCommand();
-        command.BarCode = "123456789";
-    }
+                command.FirstName = "Thiago";
+                command.LastName = "Menezes";
+                command.Document = "999999999999";
+                command.Email = "thiagocwbpr@gmail.com";
+                command.BarCode = "123456789";
+                command.BoletoNumber = "1234567897";
+                command.PaymentNumber= "212454"; // numero interno de controle. 8 digitos.
+                command.PaidDate = DateTime.Now;
+                command.ExpireDate = DateTime.Now.AddMonths(1);
+                command.Total = 60;
+                command.TotalPaid = 60;
+                command.Payer = "WAYNE CORP";
+                command.PayerDocument = "12345678911";
+                command.PayerDocumentType = EDocumentType.CPF;
+                command.PayerEmail= "manoleiro@gmail.com"; 
+                command.Street = "Rio Nilo";
+                command.Number= "98"; 
+                command.Neighborhood = "Rio";
+                command.City = "Rio de Janeiro";
+                command.State = "Rio de Janeiro";
+                command.Country = "Brasil";
+                command.ZipCode = "55";
+
+                handler.Handle(command);
+                Assert.AreEqual(false, handler.IsValid);
+            }
 }
